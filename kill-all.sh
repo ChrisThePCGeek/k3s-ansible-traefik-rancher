f9aea8ab165c52ac4fb287a1a3967be8b22d2563
ThePCGeek <thepcgeek1776@gmail.com>
2023-11-01 17:33:25 -0400

    Updates and add internal/ext traefik

--- kill-all.sh
+++ kill-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook reset.yml -i inventory/my-cluster/hosts.ini
+ansible-playbook reset.yml
 #ansible-playbook reset.yml -i inventory/my-cluster/hosts.ini --ask-pass --ask-become-pass
